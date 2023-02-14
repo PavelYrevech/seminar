3a110b749c6dde977fdcd0ba4a5963447cdbdae6
PavelYrevich <dj.2009@mail.ru>
2023-02-14 11:11:15 +0300

    Разбор заданий семинара

---
+++ seminar1/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 1. Программа по нахождению квадрата
+// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int quad = num * num;
+// Console.WriteLine($" quad of {num} is {quad}");
+// Задача 2. Программа которая принимает на вхлд два числа и проверяет является ли первое число квадратом второго
+
+// Console.WriteLine("Input first number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int quad = num2 * num2;
+
+// if (quad == num1)
+
+//  {Console.WriteLine("First number is quad number");}
+
+// else
+
+//  {Console.WriteLine("First number is not quad number");}
+
+// Задача 3. Программа которая принимает на вход число N, а на выходе показывает все целые числа в промежутке от -N до N
+
+// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int current = num * -1;
+
+// while (current <= num)
+// {
+// Console.Write(current +" ");
+//  current++;
+// }
+
+// Задача 4. напишите программу которая на вход принимкет трёхзначное число, а на выходе показывает последнюю цифру этого числа
+
+
+// Console.WriteLine("Enter any three-digit number");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int result = num %10;
+
+// Console.WriteLine($"Last digit of {num} is {result}");
+
+
